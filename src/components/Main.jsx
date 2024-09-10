59cabafb42caa42d6c16e43e5bae743e85204c8f
tiagoribeiro2001 <ribeiro.tiago2001@gmail.com>
2024-09-10 19:12:08 +0100

    Main acabado

---
+++ src/components/Main.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import fundo from "./../assets/fundo.png";
+import { TypeAnimation } from "react-type-animation";
+import { FaLinkedin, FaGithub, FaOrcid } from "react-icons/fa";
+
+const Main = () => {
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover object-left"
+        src={fundo}
+        alt="/"
+      ></img>
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4l font-bold text-gray-800">
+            I'm Tiago Ribeiro
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+            I'm a
+            <TypeAnimation
+              sequence={[
+                "Software Engineer",
+                2000,
+                "Developer",
+                2000,
+                "Tech Enthusiast",
+                2000,
+              ]}
+              wrapper="div"
+              cursor={true}
+              style={{ fontSize: "1em", paddingLeft: "5px" }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
+            <a
+              href="https://www.linkedin.com/in/tiago-ribeiro-265257270/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="cursor-pointer" size={20} />
+            </a>
+            <a
+              href="https://github.com/tiagoribeiro2001"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="cursor-pointer" size={20} />
+            </a>
+            <a
+              href="https://orcid.org/0009-0006-2961-8236"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaOrcid className="cursor-pointer" size={20} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -66,31 +66,31 @@ const Sidenav = () => {
         <div className="flex flex-col">
           <a
             href="#main"
-            className="rounded-full shadow-lg bg-grey-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
           </a>
           <a
             href="#work"
-            className="rounded-full shadow-lg bg-grey-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
           </a>
           <a
             href="#projects"
-            className="rounded-full shadow-lg bg-grey-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
           </a>
           <a
             href="#main"
-            className="rounded-full shadow-lg bg-grey-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
           </a>
           <a
             href="#contact"
-            className="rounded-full shadow-lg bg-grey-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineMail size={20} />
           </a>
