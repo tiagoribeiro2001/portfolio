814720c4f64a8d93c9d1ac3a68fb51b5fb0078e7
tiagoribeiro2001 <ribeiro.tiago2001@gmail.com>
2024-09-10 18:27:16 +0100

    Navbar feita

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio-deploy/",
+  server: {
+    watch: {
+      usePolling: true
+    }
+  },
 })
