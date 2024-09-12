fd37b6d3495b28a76b6fa313aeeb15d75758adbd
tiagoribeiro2001 <ribeiro.tiago2001@gmail.com>
2024-09-12 19:02:06 +0100

    Projects adicionado

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import andaaqui from "./../assets/andaaqui.png";
+import crowdflow from "./../assets/crowdflow.png";
+import ghost from "./../assets/ghost.png";
+import rasbet from "./../assets/rasbet.png";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        Projects
+      </h1>
+      <p className="text-center py-8">
+        Here are some of my projects, showcasing the technologies and tools I've
+        worked with. Take a look at the work I've done so far.
+      </p>
+      <div className="grid sm:grid-cols-2 gap-12">
+        <ProjectItem
+          img={crowdflow}
+          title="Crowdflow"
+          details="React + Azure"
+          redirect="https://www.crowdflow.pt"
+        />
+        <ProjectItem
+          img={andaaqui}
+          title="Andaaqui"
+          details="React + Flask + Google Maps API"
+          redirect="https://github.com/tiagoribeiro2001/andaaqui"
+        />
+        <ProjectItem
+          img={ghost}
+          title="Ghost"
+          details="Google Cloud + Ansible + Kubernetes"
+          redirect="https://github.com/tiagoribeiro2001/Projeto-de-ASCN"
+        />
+        <ProjectItem
+          img={rasbet}
+          title="RASBet"
+          details="React + Python + Java"
+          redirect="https://github.com/tiagoribeiro2001/Projeto-de-Requisitos-e-Arquiteturas-de-Software-RASBet"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WorkItem = ({ year, title, duration, details }) => {
   return (
     <ol className="flex felx-col md:flex-row relative border-l border-stone-200">
-      <l1 className="mb-10 ml-4">
+      <li className="mb-10 ml-4">
         <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white" />
         <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
           <span className="inline-block px-2 py-1 font-semibold text-white bg-[#001b5e] rounded-md">
@@ -15,7 +15,7 @@ const WorkItem = ({ year, title, duration, details }) => {
           </span>
         </p>
         <p className="my-2 text-base font-normal text-stone-500">{details}</p>
-      </l1>
+      </li>
     </ol>
   );
 };
