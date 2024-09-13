4277d884a037d07c85185286328324cc269a41e1
tiagoribeiro2001 <ribeiro.tiago2001@gmail.com>
2024-09-13 14:57:10 +0100

    Fix na navbar

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
   return (
     <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
-      <p className="text-center py-8">
+      <p className="text-lg text-center py-8">
         Here are some of my projects, showcasing the technologies and tools I've worked with. Take a look at the work I've done so far.
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
